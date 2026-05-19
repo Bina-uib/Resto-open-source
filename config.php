002f008c20ddc0ae7cff182ce28aa104ff429627
randikhsan <randifahrul54@gmail.com>
2026-05-19 18:52:37 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "warung_os";
- $conn = new mysqli($host, $user, $pass, $db);
- if ($conn->connect_error) {
-die("Koneksi gagal: " . $conn->connect_error);
+$conn = new mysqli("localhost", "root", "", "warung_os");
+
+if ($conn->connect_error) {
+    die("Koneksi gagal");
 }
- $conn->set_charset("utf8");
 ?>
